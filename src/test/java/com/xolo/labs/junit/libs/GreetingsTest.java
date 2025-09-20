0cb6ac9e28441c665c74d20c111e4971b7bb5836
wichis <wichis@users.noreply.github.com>
2025-09-20 09:32:57 -0600

    Enhance GreetingsTest with Javadoc comments
    Added documentation comments to the GreetingsTest class and its methods.

--- src/test/java/com/xolo/labs/junit/libs/GreetingsTest.java
+++ src/test/java/com/xolo/labs/junit/libs/GreetingsTest.java
@@ -1,3 +1,19 @@
+/**
+ * GreetingsTest.java
+ *
+ * Simple JUnit 5 test class for verifying the Greetings class.
+ * 
+ * This repository demonstrates basic usage of JUnit 5 for simple unit tests.
+ * 
+ * - The Greetings class provides a method to return greetings in different languages.
+ * - We use JUnit 5's @Test annotation and assertions to test that Greetings returns the correct string for each LenguageType.
+ * - The tests are straightforward and don't cover complex scenarios.
+ * 
+ * To run the tests, use your IDE or 'mvn test' if using Maven.
+ * 
+ * For more information, see the README of this repository.
+ */
+
 package com.xolo.labs.junit.libs;
 
 import com.xolo.labs.junit.libs.types.LenguageType;
@@ -5,8 +21,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for the Greetings class.
+ */
 class GreetingsTest {
 
+    /**
+     * Test the saludar method for different language types.
+     * Uses simple assertions to verify expected results.
+     */
     @Test
     void saludar() {
         Greetings greetings = new Greetings();
